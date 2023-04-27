ca3e22a84790cada2d1d6fca89831007050400de
Hun Win Thu <mr.hunwinthu@gmail.com>
2023-04-27 16:50:13 +0700

    second commit

--- testclass.cs
+++ testclass.cs
@@ -6,11 +6,14 @@ using System.Text;
 public class ProgramTests
 {
    [Theory]
-   [InlineData("4433555 555666#", "HELLO")]
+   
    [InlineData("77776667777#", "SOS")]
    [InlineData("44883336**6647778*999#", "HUNGRY")]
-
+   [InlineData("33#", "E")]
+   [InlineData("227*#", "B")]
+   [InlineData("4433555 555666#", "HELLO")]
    [InlineData("8 88777444666*664#", "TURING")]
+
    public void PrintSentence_ReturnsExpectedOutput(string input, string expectedOutput)
    {
       // Arrange
